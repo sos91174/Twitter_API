e6fda49a0f42da3d8ac08ea56c63b2460cc8888c
sos9174 <sos9174@gmail.com>
2020-09-21 23:46:31 +0800

    Twitter_API

---
+++ src/main/java/JavaRestTweet.java
@@ -0,0 +1,66 @@
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+
+
+public class JavaRestTweet {
+    static String consumerKeyStr = "UUBuGCkkU70794Kzq0LHFf7nE";
+    static String consumerSecretStr = "ldLMPzqp1giyIF8BlreqH9vKLQAn5hmaXxynyZRM9pw5aT3CeR";
+    static String accessTokenStr = "1262585608897089541-4f9SgVjmjl3XhehtS17ZlRfbeKmBoI";
+    static String accessTokenSecretStr = "JMzoICndWtxX0r0bbIFqZn4c6QVTuS06kDwEVaUlkawp9";
+
+
+    public static void main(String[] args) {
+
+        try {
+            Twitter twitter = new TwitterFactory().getInstance();
+
+            twitter.setOAuthConsumer(consumerKeyStr, consumerSecretStr);
+            AccessToken accessToken = new AccessToken(accessTokenStr,
+                    accessTokenSecretStr);
+
+            twitter.setOAuthAccessToken(accessToken);
+
+            twitter.updateStatus("Java test OK!");
+
+            System.out.println("Successfully updated the status in Twitter.");
+        } catch (TwitterException te) {
+            te.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+//    public static void main(String[] args) throws Exception {
+//        OAuthConsumer oAuthConsumer = new CommonsHttpOAuthConsumer(consumerKeyStr,
+//                consumerSecretStr);
+//        oAuthConsumer.setTokenWithSecret(accessTokenStr, accessTokenSecretStr);
+//
+//        HttpPost httpPost = new HttpPost(
+//                "http://api.twitter.com/1.1/statuses/update.json?status=Hello%20Twitter%20World.");
+//
+//        oAuthConsumer.sign(httpPost);
+//
+//        HttpClient httpClient = new DefaultHttpClient();
+//        HttpResponse httpResponse = httpClient.execute(httpPost);
+//
+//        int statusCode = httpResponse.getStatusLine().getStatusCode();
+//        System.out.println(statusCode + ':'
+//                + httpResponse.getStatusLine().getReasonPhrase());
+//        System.out.println(IOUtils.toString(httpResponse.getEntity().getContent()));
+//
+//    }
+}
